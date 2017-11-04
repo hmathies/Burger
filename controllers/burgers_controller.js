9000ea4d2466e47e595a1ec741b421f6a464faa2
Heather <hlmathies@gmail.com>
2017-11-04 18:13:39 -0400

    Set Up Controllers folder

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,48 @@
+//importing our packages
+var express = require('express');
+var router = express.Router();
+//importing our model
+var burger = require('../models/burger.js');
+
+//the `router` for the app
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/cats", function(req, res) {
+  burger.create([
+    "??", "??"
+  ], [
+    req.body.??, req.body.??
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    ??: req.body.??
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// export the `router`
+module.exports = router;
